e355ad718f0073991d122d37c0cefb096a625482
iamxebakahn <zebadwight@gmail.com>
2025-05-20 14:07:51 +0530

    Add files via upload

---
+++ Q3AP_Beginner_More_problems_on_Loops_&_Patterns.java
@@ -0,0 +1,81 @@
+/*Problem Description
+
+Take T (number of test cases) as input.
+For each test case, take integer N as input and Print the sum of digits of that number.
+
+
+Problem Constraints
+
+1 <= T <= 100
+0 <= N <= 100000000
+
+
+Input Format
+
+The first line is the number T which denotes the total number of test cases.
+Next T lines contain an integer N for which you have to print the sum of digits.
+
+
+Output Format
+
+For T different Numbers, Print the sum of digits in separate lines.
+
+
+Example Input
+
+Input 1:
+2
+81
+145
+
+Input 2:
+2
+201
+1000
+
+
+Example Output
+
+Output 1:
+9
+10
+
+Output 2:
+3
+1 */
+import java.lang.*;
+import java.util.*;
+
+public class Q3AP_Beginner_More_problems_on_Loops_&_Patterns{
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+       Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T>0)
+        {
+            int N=sc.nextInt();
+            
+            
+        int digit, sum=0;
+
+            while(N>0)
+            {
+
+                digit=N%10;
+                sum+=digit;
+                N/=10;
+             
+            }
+
+        
+
+        System.out.print(sum);
+        System.out.println();
+        
+            T--;
+        } 
+    }
+}
